f8b366a12c04398e8f60182720a3a8363ab130b2
sriharinarayanasamy2007-gif <sriharinarayanasamy2007@gmail.com>
2026-07-11 11:42:53 +0530

    Add files via upload

---
+++ gray.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread(r"C:\Users\Srihari\OneDrive\Desktop\Downloads\tree.jpg")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Original Image", img)
+cv2.imshow("Grayscale Image", gray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
