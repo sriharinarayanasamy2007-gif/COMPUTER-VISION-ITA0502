9ca677c7a9e0faf02583734fe8626e0497ca8ec2
sriharinarayanasamy2007-gif <sriharinarayanasamy2007@gmail.com>
2026-08-13 11:27:28 +0530

    Add files via upload

---
+++ LAB PROGRAM/exp 33.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread(r"C:\Users\Srihari\OneDrive\Desktop\Downloads\exp12.jpg")
+
+kernel = np.ones((5, 5), np.uint8)
+
+gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+
+cv2.imshow("Original Image", img)
+cv2.imshow("Morphological Gradient", gradient)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ LAB PROGRAM/exp 34.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread(r"C:\Users\Srihari\OneDrive\Desktop\Downloads\exp12.jpg")
+
+kernel = np.ones((5, 5), np.uint8)
+
+top_hat = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, kernel)
+
+cv2.imshow("Original Image", img)
+cv2.imshow("Top Hat", top_hat)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
